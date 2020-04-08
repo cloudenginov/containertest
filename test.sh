e9100d27e9bc1bc0250becb6c28f167af8e6c8de
cloudenginov <58642239+cloudenginov@users.noreply.github.com>
2020-04-08 14:43:48 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,11 +2,11 @@
 
 apk add --no-cache git
 
-cat /src/hello.txt
+cat $HOME/hello.txt
 
 echo "Hello from inside container" > /src/container.txt
 
 mkdir -p /root/src/proj
 
-cd /root/src/proj
+cd $HOME/src/proj
 git clone https://github.com/cloudenginov/containertest.git
